e57cca2f79f4ec6cb92772f6fe0f532566032fa4
GaganTut <gstut@live.com>
2017-02-03 21:27:49 -1000

    Number SEVEN is absolutely done.

--- exercises.js
+++ exercises.js
@@ -135,8 +135,10 @@ console.log(checkSum(sum));
  * This function will return the string "Last night I dreamt that I ate `x` Taco Bell soft tacos."  Where `x` is the value when you invoke the function. Invoke this function using the variable `difference`.
  * Console.log your results.
 */ 
-
-
+function checkDifference(x){
+	return "Last night, I dreamt that I ate " + x + " Taco Bell soft tacos.";
+}
+console.log(checkDifference(difference));
 
 /*
  * #8
